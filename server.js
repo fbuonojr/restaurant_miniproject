a4d8e1a0ab9a361615fbb88179316978fbc592c7
chrisfaux95 <chrisfaux95@gmail.com>
2020-08-18 13:43:16 -0400

    fixed links and added express

--- server.js
+++ server.js
@@ -19,30 +19,32 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 })
 
-app.get("/add", function(req, res) {
+app.get("/add", function (req, res) {
     res.sendFile(path.join(__dirname, "add.html"));
 })
 
-app.get("/tables", function(req, res){
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 })
 
 //ROUTES FOR API TABLES
-app.get("/api/tables", function(req, res){
+app.get("/api/tables", function (req, res) {
     return res.json(tables);
 });
 
-app.get("/api/waitlist", function(req, res){
+app.get("/api/waitlist", function (req, res) {
     return res.json(waitlist);
 })
 
 //POST METHODS
 
-app.post("/api/tables", function(req, res){
+app.post("/api/tables", function (req, res) {
     var newGuest = req.body;
-    console.log(newGuest);
-    addGuest(newGuest);
-    res.json(newGuest);
+    if (newGuest != null) {
+        console.log(newGuest);
+        let onReservations = addGuest(newGuest);
+        res.json(onReservations);
+    }
 })
 
 
@@ -56,14 +58,16 @@ app.listen(PORT, function () {
 function addGuest(guest) {
     if (tables.length > MAX_GUESTS) {
         waitlist.push(guest);
+        return false;
     } else {
-        tables.push(guest);
+        tables.push(guest)
+        return true;
     }
 }
 
 //Guest class
 class Guest {
-    constructor(id, name, email, phone){
+    constructor(id, name, email, phone) {
         this.id = id;
         this.name = name;
         this.email = email;
